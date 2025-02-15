77c4cc1ab9194934a48e5088e2355fb40cc46966
Nastyaa8 <152227415+Nastyaa8@users.noreply.github.com>
2025-02-15 17:25:19 +0300

    Add files via upload

---
+++ PolishNotation.h
@@ -0,0 +1,8 @@
+#pragma once
+#pragma once
+#include "LEX.h"
+
+namespace PolishNotation {
+	bool PolishNotation(int lextable_pos, LT::LexTable& lextable, IT::IdTable& idtable);
+	void DoPolish(LEX::LEX t);
+}
